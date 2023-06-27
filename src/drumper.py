9ed75b17123e4cb12b58de15f1a7fe96ec496e47
EneRgYCZ <catalin.zaharia2013@gmail.com>
2023-06-27 18:57:47 +0200

    Updated results

--- src/drumper.py
+++ src/drumper.py
@@ -2,12 +2,51 @@ import os
 import numpy as np
 from keras.models import load_model
 from midiutil import MIDIFile
+import mido
+import random
+
+
+def preprocess_midi(midi_file):
+    # Load the MIDI file
+    mid = mido.MidiFile(midi_file)
+
+    # Initialize an empty list to hold the notes and velocities
+    notes = []
+    velocities = []
+
+    # Iterate over all the messages in the MIDI file
+    for msg in mid:
+        # Check if the message is a 'note_on' message
+        if msg.type == 'note_on':
+            # Append the note and velocity to their respective lists
+            notes.append(msg.note)
+            velocities.append(msg.velocity)
+
+    # Normalize the notes and velocities
+    notes = np.array(notes) / 127.0
+    velocities = np.array(velocities) / 127.0
+
+    # Stack the notes and velocities together
+    preprocessed_data = np.column_stack((notes, velocities))
+
+    return preprocessed_data
 
 # Load the model
 model = load_model('../results/drummer.h5')
 
 # Create a seed sequence
-seed = np.random.rand(1, 16, 2)
+midi_data = mido.MidiFile('../data/midi_songs/Songs/All_Star.mid')
+
+# Preprocess the MIDI data
+preprocessed_data = preprocess_midi("../data/midi_songs/Songs/Its_My_Life.mid")
+
+# Create the seed sequence
+seed = preprocessed_data[:16]
+
+# Reshape the seed sequence to match the input shape of your model
+seed = seed.reshape(1, 16, 2)
+
+print (seed)
 
 # Define the number of steps to generate
 num_steps = 50
@@ -43,31 +82,38 @@ midi = MIDIFile(1)
 # 9 is the MIDI program number for a drum kit
 midi.addProgramChange(0, 9, 1, 9)
 
+beat_time = [2, 3]
+
 # Add the generated notes to the MIDI file
 for i, note in enumerate(generated):
     pitch, velocity = note[0]
     # MIDI note number and velocity should be integers
     pitch = int(pitch)
-    velocity = int(velocity)
+    velocity = int(velocity) + 20
+    print (pitch)
+    print (velocity)
+    beat = random.choice(beat_time)
+    print (beat)
+    print ()
     # Add the note to the MIDI file
     midi.addNote(track=0, channel=9, pitch=pitch,
-                 time=i, duration=1, volume=velocity)
+                 time=i / 3, duration=1, volume=velocity)
 
 # Write the MIDI file to disk
-with open("../results/output.mid", "wb") as output_file:
+with open("../results/output2.mid", "wb") as output_file:
     midi.writeFile(output_file)
 
     # Path to your SoundFont file
 soundfont_path = "da.sf2"
 
 # Path to your MIDI file
-midi_path = "../results/output.mid"
+midi_path = "../results/output2.mid"
 
 # Path to the output WAV file
-wav_path = "../results/output.wav"
+wav_path = "../results/output2.wav"
 
 # Path to the output MP3 file
-mp3_path = "../results/output.mp3"
+mp3_path = "../results/output2.mp3"
 
 # Use FluidSynth to convert the MIDI file to a WAV file
 os.system(
